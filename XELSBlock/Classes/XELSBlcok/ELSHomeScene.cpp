b20e6f36a7dd7443a8b2ff94c35de9488c06a1b7
Will <wxx1035@163.com>
2022-09-16 21:15:52 +0800

    创建项目贪吃蛇

---
+++ XELSBlock/Classes/XELSBlcok/ELSHomeScene.cpp
@@ -0,0 +1,8 @@
+//
+//  ELSHomeScene.cpp
+//  XELSBlock
+//
+//  Created by Will Young on 2022/9/15.
+//
+
+#include "ELSHomeScene.hpp"
---
+++ XELSBlock/Classes/XELSBlcok/ELSHomeScene.hpp
@@ -0,0 +1,13 @@
+//
+//  ELSHomeScene.hpp
+//  XELSBlock
+//
+//  Created by Will Young on 2022/9/15.
+//
+
+#ifndef ELSHomeScene_hpp
+#define ELSHomeScene_hpp
+
+#include <stdio.h>
+
+#endif /* ELSHomeScene_hpp */
